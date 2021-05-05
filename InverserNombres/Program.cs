dd5d525035143f21989f8a7091e87a9b09786d5a
samira <sboudiaf@arfp.asso.fr>
2021-05-05 11:32:13 +0200

    ajout des modif dans projet inverserNombres

--- InverserNombres/Program.cs
+++ InverserNombres/Program.cs
@@ -6,29 +6,26 @@ namespace InverserNombres
     {
         static void Main(string[] args)
         {
-
-            //inverser deux nombres
-            inverserNombres();
-        }
-
-
-        //Lire 2 nombres a et b. Mettre le contenu de a dans b et celui de b dans a
-        public static string inverserNombres()
-        {
-
-            int nombreTemporaire;
             Console.WriteLine("Entrez un premier nombre");
             int nombre1 = Int32.Parse(Console.ReadLine());
 
             Console.WriteLine("Entrez un deuxième nombre");
             int nombre2 = Int32.Parse(Console.ReadLine());
+            //inverser deux nombres
+            Console.WriteLine("premier nombre devient " + nombre1 + " - deuxième nombre devient " + nombre2);
 
-                nombreTemporaire = nombre1;
-                nombre1 = nombre2;
-                nombre2 = nombreTemporaire;
+            InverserNombres(ref nombre1, ref nombre2, out int nombreTemporaire);
 
-                return "premier nombre : " + nombre1 + " - deuxième nombre : " + nombre2;
+            Console.WriteLine("premier nombre devient " + nombre1 + " - deuxième nombre devient " + nombre2);
+        }
 
+        //Lire 2 nombres a et b. Mettre le contenu de a dans b et celui de b dans a
+        public static void InverserNombres(ref int a, ref int b, out int temporaire)
+        {
+
+            temporaire = a;
+            a = b;
+            b = temporaire;
         }
     }
 }
